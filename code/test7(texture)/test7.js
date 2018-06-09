d023dae0e901e9ce58b7c020a8a7cc2fd3cfe427
Mao <33150690+egino135@users.noreply.github.com>
2018-06-10 02:16:21 +0800

    debug

--- code/test7(texture)/test7.js
+++ code/test7(texture)/test7.js
@@ -231,6 +231,7 @@ function main()
 	img.crossOrigin = "anonymous";
 	img.addEventListener('load', function() 
 	{
+		console.log(!!!);
 		img.crossOrigin = "anonymous";
 		gl.bindTexture(gl.TEXTURE_2D, texture);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);

99cd70698d87f7271224d96e6ee797a98d07b953
Mao <33150690+egino135@users.noreply.github.com>
2018-06-19 15:57:21 +0800

    Update test8.js

--- js/test8.js
+++ js/test8.js
@@ -189,7 +189,7 @@ function loadImageAndCreateTextureInfo(gl, url)
 	gl.bindTexture(gl.TEXTURE_2D, tex);
 	// Fill the texture with a 1x1 blue pixel.
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-				  new Uint8Array([0, 0, 100, 255]));
+				  new Uint8Array([0, 0, 255, 255]));
 
 	// let's assume all images are not a power of 2
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -297,7 +297,7 @@ function loadImg2Texture(gl, path)
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	// Fill the texture with a 1x1 blue pixel.
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-				  new Uint8Array([0, 0, 255, 255]));
+				  new Uint8Array([0, 0, 100, 255]));
 
 	// let's assume all images are not a power of 2
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
